e79c41b5e4ef7ef51e26fea1f8ec87d50ca45273
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-09-27 02:48:02 +0530

    Added: testfile on UC-4

--- testfile.sh
+++ testfile.sh
@@ -5,15 +5,17 @@ echo "---------------------------------- Welcome To Employee Wage Compution ----
 isPresent=1
 
 randomNumber=$((RANDOM%2))
+caseChoice=$randomNumber
+read -p "To use case four case statement:"caseChoice
 
 #TO CHECK THE EMPLOYEE PRESENT AND ABSENT
 
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
-else
-	echo "Employee Absent"
-fi
+
+case "$caseChoice" in
+0)echo "To calculate Daily Employee Wage"
 
 function calculateDailyEmployeeWage(){
 	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
@@ -22,7 +24,20 @@ function calculateDailyEmployeeWage(){
 	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
 }
 calculateDailyEmployeeWage
+;;
+1)echo "To calculate part time Employee and wage"
+
+function partTimeEmployeeAndWage(){
+		read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+		read -p "To part time hour is :" isFullDayHour
+		calculatePartTimeEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+		echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage "rupees"
+			}
 partTimeEmployeeAndWage
+;;
+*)echo "No Choice"
+;;
+esac
 else
    echo "Employee Absent"
  fi
