664c3a7b536a1fa8d7dcd1695701cb1b9e244c97
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-09-27 01:55:49 +0530

    Added: testfile on UC-1

--- testfile.sh
+++ testfile.sh
@@ -2,6 +2,17 @@
 
 echo "---------------------------------- Welcome To Employee Wage Compution --------------------------------------------"
 
+isPresent=1
 
+randomNumber=$((RANDOM%2))
+
+#TO CHECK THE EMPLOYEE PRESENT AND ABSENT
+
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
 
 
